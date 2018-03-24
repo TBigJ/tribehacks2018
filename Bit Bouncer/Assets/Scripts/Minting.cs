ac89950a209afb3e8f412b12ab317afae8ba3ca7
Yang Zhang <yangzhang333@gmail.com>
2018-03-24 14:38:12 -0400

    Basic Platform
    This is the basic platform of the game, player that can jump and an enemy spawner

---
+++ Bit Bouncer/Assets/Scripts/Minting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minting : MonoBehaviour {
+
+		public GameObject[] enemySpawn;
+		public Transform enemyPrefab;
+		public bool creating = true;
+
+		void Update()
+		{
+		if (creating = true)
+			{
+				Debug.Log (creating);
+				Instantiate(enemyPrefab, transform.position, transform.rotation);
+				creating = false;
+			}
+		}
+}
---
+++ Bit Bouncer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public bool isJumping = false; 
+	public float speed = 1;
+	public float longJump = 10;
+	public float shortJump = 8;
+	public float gravity = 15;
+	private Vector3 moveDirection = Vector3.zero;
+
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal")*2.5f, 0, 0);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("longJump"))
+				startLongJump ();
+			if (Input.GetButton ("shortJump"))
+				startShortJump ();
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	void startShortJump()
+	{
+		moveDirection.y = shortJump;
+	}
+
+	void startLongJump()
+	{
+		moveDirection.y = longJump;
+	}
+
+}
